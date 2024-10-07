604d5c24001cd3c292025ac5965ea1bdf8964a13
KyleBoBa <kyle.bostrombalthazar@elev.norrkoping.se>
2024-10-07 03:56:01 +0200

    sure

--- player.py
+++ player.py
@@ -12,29 +12,5 @@ class Player:
     def __str__(self):
         return f"{self.name} har spelat {self.total_matches} och vunnit {self.won_matches}"
 
-#dessa metoder är inte funktionella vid nuläget, möjligtvis tilläggs
-    def change_win_early(self, win_early):
-        self.win_early = win_early
-        return self.win_early
-    
-    def change_win_mid(self, win_mid):
-        self.win_mid = win_mid
-        return self.win_mid
-    
-    def change_win_late(self, win_late):
-        self.win_late = win_late
-        return self.win_late
-    
-    def change_won_matches(self, won_matches):
-        self.won_matches = won_matches
-        return self.won_matches
-    
-    def change_total_matches(self, total_matches):
-        self.total_matches = total_matches
-        return self.win_early
-    
-    def adjust_elo(self, updated_elo):
-        self.elo = updated_elo
-    
     def assign_team(self, team):
         self.team = team
